7e0e92743b7514e71948a7cb455de4c158a50196
DmnNg <49575407+DmnNg@users.noreply.github.com>
2021-09-30 15:51:46 +1000

    Added DATABASE_URL

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,8 @@ DB_NAME = "database.db"
 def create_app():
 	app = Flask(__name__)
 	app.config['SECRET_KEY'] = 'secretkey'
-	app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+	app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	db.init_app(app)
 
 	from .views import views
