0f38275884f99caec773d8400da27bfc7cf86722
DmnNg <49575407+DmnNg@users.noreply.github.com>
2021-10-04 19:51:56 +1100

    Update models.py

--- website/models.py
+++ website/models.py
@@ -21,6 +21,7 @@ class Result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     percentage = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
