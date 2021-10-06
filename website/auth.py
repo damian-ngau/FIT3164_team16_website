9bc8372805cbcd3712cdb9e3fc69762e7afe84f6
DmnNg <49575407+DmnNg@users.noreply.github.com>
2021-10-07 02:58:21 +1100

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -145,7 +145,7 @@ def predict_label(img_path):
 @login_required
 def get_output():
 	if request.method == 'POST':
-		if 'my_image'not in request.files:
+		if 'my_image' not in request.files:
 			flash('No Image Selected', category='error')
 			return render_template("upload_image.html", user=current_user)
 		
