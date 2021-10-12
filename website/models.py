96eea78418fa9458dc7619a053ece44eb0fa078a
DmnNg <49575407+DmnNg@users.noreply.github.com>
2021-10-13 03:40:40 +1100

    Added comments

--- website/models.py
+++ website/models.py
@@ -2,6 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+# database table for User
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
@@ -9,14 +10,14 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(150))
     patients = db.relationship('Patient')
 
-
+# database table for Patient
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(150))
     results = db.relationship('Result')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
+# database table for Result
 class Result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     note = db.Column(db.String(10000))
